b8dffeafebcbe542c2f1cb86a386d0c2eece7dc2
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-12-31 22:42:26 +0900

    Update .zsh

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,50 @@
+alias tmux='tmux -2'
+
+alias -s js=node
+alias -s py=python
+alias -s txt=cat
+
+if [ $uname = 'Darwin' ]; then
+    alias ls='ls -G'
+    alias brew="env PATH=${PATH/:$HOME\/\.pyenv\/shims/} brew"
+
+    alias canary='open -a Google\ Chrome\ Canary'
+    alias chrome='open -a Google\ Chrome'
+    alias firefox='open -a Firefox'
+    alias safari='open -a Safari'
+
+    alias rm-open-in-duplicates="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
+
+    alias -s {png,jpg,bmp,pdf,PNG,JPG,BMP,PDF}='open -a Preview'
+
+elif [ $uname = 'Linux' ]; then
+    alias ls='ls --color'
+
+    if [ -x "`which gnome-open 2>/dev/null`" ]; then
+        alias open='gnome-open'
+    fi
+    if [ -x "`which xsel 2>/dev/null`" ]; then
+        alias pbcopy='xsel --clipboard --input'
+        alias pbpaste='xsel --clipboard --output'
+    fi
+
+    alias -s {png,jpg,bmp,PNG,JPG,BMP}=eog
+fi
+
+# 各種圧縮ファイルの解凍
+function extract() {
+    case $1 in
+        *.tar.gz|*.tgz) tar xzvf $1;;
+        *.tar.xz) tar Jxvf $1;;
+        *.zip) unzip $1;;
+        *.lzh) lha e $1;;
+        *.tar.bz2|*.tbz) tar xjvf $1;;
+        *.tar.Z) tar zxvf $1;;
+        *.gz) gzip -dc $1;;
+        *.bz2) bzip2 -dc $1;;
+        *.Z) uncompress $1;;
+        *.tar) tar xvf $1;;
+        *.arj) unarj $1;;
+    esac
+}
+alias -s {gz,tgz,zip,lzh,bz2,tbz,Z,tar,arj,xz}=extract
---
+++ .zsh/completion.zsh
@@ -0,0 +1,70 @@
+# pip zsh completion start
+function _pip_completion {
+    local words cword
+    read -Ac words
+    read -cn cword
+    reply=( $( COMP_WORDS="$words[*]" \
+               COMP_CWORD=$(( cword-1 )) \
+               PIP_AUTO_COMPLETE=1 $words[1] ) )
+}
+compctl -K _pip_completion pip
+# pip zsh completion end
+
+###-begin-npm-completion-###
+#
+# npm command completion script
+#
+# Installation: npm completion >> ~/.bashrc  (or ~/.zshrc)
+# Or, maybe: npm completion > /usr/local/etc/bash_completion.d/npm
+#
+
+COMP_WORDBREAKS=${COMP_WORDBREAKS/=/}
+COMP_WORDBREAKS=${COMP_WORDBREAKS/@/}
+export COMP_WORDBREAKS
+
+if type complete &>/dev/null; then
+  _npm_completion () {
+    local si="$IFS"
+    IFS=$'\n' COMPREPLY=($(COMP_CWORD="$COMP_CWORD" \
+                           COMP_LINE="$COMP_LINE" \
+                           COMP_POINT="$COMP_POINT" \
+                           npm completion -- "${COMP_WORDS[@]}" \
+                           2>/dev/null)) || return $?
+    IFS="$si"
+  }
+  complete -F _npm_completion npm
+elif type compdef &>/dev/null; then
+  _npm_completion() {
+    si=$IFS
+    compadd -- $(COMP_CWORD=$((CURRENT-1)) \
+                 COMP_LINE=$BUFFER \
+                 COMP_POINT=0 \
+                 npm completion -- "${words[@]}" \
+                 2>/dev/null)
+    IFS=$si
+  }
+  compdef _npm_completion npm
+elif type compctl &>/dev/null; then
+  _npm_completion () {
+    local cword line point words si
+    read -Ac words
+    read -cn cword
+    let cword-=1
+    read -l line
+    read -ln point
+    si="$IFS"
+    IFS=$'\n' reply=($(COMP_CWORD="$cword" \
+                       COMP_LINE="$line" \
+                       COMP_POINT="$point" \
+                       npm completion -- "${words[@]}" \
+                       2>/dev/null)) || return $?
+    IFS="$si"
+  }
+  compctl -K _npm_completion npm
+fi
+###-end-npm-completion-###
+
+# The next line enables bash completion for gcloud.
+if [ -e '~/Development/google-cloud-sdk/completion.zsh.inc' ]; then
+    source '~/Development/google-cloud-sdk/completion.zsh.inc'
+fi
