4c4e19fc52e51a5e92d5ece67d5db60066c2dc13
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-01-01 18:35:35 +0900

    Fix zsh history search with peco

--- .zsh/peco.zsh
+++ .zsh/peco.zsh
@@ -6,9 +6,8 @@ if [ -x "`which peco 2>/dev/null`" ]; then
         else
             tac="tail -r"
         fi
-        BUFFER=$(\history -n 1 | eval $tac | peco --query "$LBUFFER")
+        BUFFER=$(\history -n 1 | eval $tac | awk '!a[$0]++' | peco --query "$LBUFFER")
         CURSOR=$#BUFFER
-        zle clear-screen
     }
     zle -N peco-select-history
     bindkey '^R' peco-select-history
