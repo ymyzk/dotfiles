d066a47922596420c921d8df401c7011021bcbc2
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-02-01 18:44:34 +0900

    Add peco-ssh-select-host

--- .zsh/peco.zsh
+++ .zsh/peco.zsh
@@ -1,5 +1,5 @@
 if _command_exists peco; then
-    function peco-select-history() {
+    function _peco-select-history() {
         local tac
         if _command_exists tac; then
             tac="tac"
@@ -9,6 +9,16 @@ if _command_exists peco; then
         BUFFER=$(\history -n 1 | eval $tac | awk '!a[$0]++' | peco --query "$LBUFFER")
         CURSOR=$#BUFFER
     }
-    zle -N peco-select-history
-    bindkey '^R' peco-select-history
+    zle -N _peco-select-history
+    bindkey '^r' _peco-select-history
+
+    function _peco-ssh-select-host() {
+        local host
+        echo "$LBUFFER"
+        host=$(grep -iE '^host' ~/.ssh/config | awk '{print $2}' | peco)
+        if [ "$host" != '' ]; then
+            ssh $@ $host
+        fi
+    }
+    alias ssh-peco=_peco-ssh-select-host
 fi
