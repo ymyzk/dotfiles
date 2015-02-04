8dad377f3f15a2bf2e2d9b4db75d3ec0c4840272
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-02-04 13:41:52 +0900

    Update _send-yo

--- .zsh/yo.zsh
+++ .zsh/yo.zsh
@@ -1,8 +1,41 @@
+# YO_API_TOKEN is required.
+# Get it from https://dev.justyo.co
+
 YO_API_URL='https://api.justyo.co/yo/'
 
 function _send-yo() {
-    if [ $# -gt 0 ]; then
-        curl -s -d "api_token=$YO_API_TOKEN" -d "username=$1" $YO_API_URL
+    # Check arguments
+    if [ $# -lt 2 ]; then
+        return
+    fi
+
+    local request_command
+    local message
+    local response
+    local yo_type=$1
+    local username=$(echo $2 | tr '[:lower:]' '[:upper:]')
+
+    # Parse arguments
+    case $yo_type in
+        yo)
+            message="Yo! $username"
+            request_command="curl --silent --write-out %{http_code} --output /dev/null -d api_token=$YO_API_TOKEN -d username=$username $YO_API_URL"
+            ;;
+        *)
+            echo "Unrecognized Yo type: $yo_type" 1>&2
+            return
+            ;;
+    esac
+
+    # Send request
+    printf "[....] $message"
+    response=$(eval "$request_command")
+
+    # Show result
+    if [ $response = '200' ]; then
+        printf "\r[ OK ] $message\n"
+    else
+        printf "\r[FAIL] $message\n"
     fi
 }
 
@@ -12,7 +45,7 @@ function send-yo() {
         echo "usage: $0 username ..."
     else
         for username in $@; do
-            _send_yo $username > /dev/null
+            _send-yo 'yo' $username
         done
     fi
 }
