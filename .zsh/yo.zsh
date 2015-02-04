839e18284959f563f0317cab260ffc3fbd6f1da9
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-02-05 01:48:52 +0900

    Update yo.zsh

--- .zsh/yo.zsh
+++ .zsh/yo.zsh
@@ -3,49 +3,82 @@
 
 YO_API_URL='https://api.justyo.co/yo/'
 
-function _send-yo() {
-    # Check arguments
-    if [ $# -lt 2 ]; then
-        return
-    fi
-
+function send-yo() {
+    local location
+    local url
+    local username
+    local usernames
     local request_command
     local message
     local response
-    local yo_type=$1
-    local username=$(echo $2 | tr '[:lower:]' '[:upper:]')
+    local opt
 
     # Parse arguments
-    case $yo_type in
-        yo)
+    for opt in "$@"; do
+        case $opt in
+            '-h'|'--help')
+                echo "usage: $0 [options] username ..."
+                echo
+                echo "Options:"
+                echo "  --help, -h"
+                echo "      Show help"
+                echo "  --location {lat,lon}, -l {lat,lon}"
+                echo "      Send Yo Location"
+                echo "  --url {url}, -u {url}"
+                echo "      Send Yo Link"
+                return 1
+                ;;
+            '-l'|'--location')
+                if [[ -z "$2" ]]; then
+                    echo "requires argument"
+                    return 1
+                fi
+                location="$2"
+                shift 2
+                ;;
+            '-u'|'--url')
+                if [[ -z "$2" ]]; then
+                    echo "requires argument"
+                    return 1
+                fi
+                url="$2"
+                shift 2
+                ;;
+            -*)
+                echo "illegal option"
+                return 1
+                ;;
+            *)
+                if [[ -n "$1" ]]; then
+                    usernames+=($(echo $1 | tr '[:lower:]' '[:upper:]'))
+                    shift 1
+                fi
+                ;;
+        esac
+    done
+
+    for username in $usernames; do
+        # Prepare request
+        if [[ -n "$location" ]]; then
+            message="Yo Location! $location $username"
+            request_command="curl --silent --write-out %{http_code} --output /dev/null -d api_token=$YO_API_TOKEN -d username=$username $YO_API_URL -d 'location=$location'"
+        elif [[ -n "$url" ]]; then
+            message="Yo Link! $url $username"
+            request_command="curl --silent --write-out %{http_code} --output /dev/null -d api_token=$YO_API_TOKEN -d username=$username $YO_API_URL -d 'link=$url'"
+        else
             message="Yo! $username"
             request_command="curl --silent --write-out %{http_code} --output /dev/null -d api_token=$YO_API_TOKEN -d username=$username $YO_API_URL"
-            ;;
-        *)
-            echo "Unrecognized Yo type: $yo_type" 1>&2
-            return
-            ;;
-    esac
+        fi
 
-    # Send request
-    printf "[....] $message"
-    response=$(eval "$request_command")
+        # Send request
+        printf "[....] $message"
+        response=$(eval "$request_command")
 
-    # Show result
-    if [ $response = '200' ]; then
-        printf "\r[ OK ] $message\n"
-    else
-        printf "\r[FAIL] $message\n"
-    fi
-}
-
-function send-yo() {
-    local username
-    if [ $# -lt 1 ]; then
-        echo "usage: $0 username ..."
-    else
-        for username in $@; do
-            _send-yo 'yo' $username
-        done
-    fi
+        # Show result
+        if [ $response = '200' ]; then
+            printf "\r[ OK ] $message\n"
+        else
+            printf "\r[FAIL] $message\n"
+        fi
+    done
 }
