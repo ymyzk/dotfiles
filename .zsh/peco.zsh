e8c073fde829d20a4955cf6da7117ad658bc5460
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-07-17 23:07:48 +0900

    Style fix up

--- .zsh/peco.zsh
+++ .zsh/peco.zsh
@@ -1,23 +1,23 @@
 if _command_exists peco; then
-    function _peco-select-history() {
-        local tac
-        if _command_exists tac; then
-            tac="tac"
-        else
-            tac="tail -r"
-        fi
-        BUFFER=$(\history -n 1 | eval $tac | awk '!a[$0]++' | peco --query "$LBUFFER")
+  function _peco-select-history() {
+    local tac
+    if _command_exists tac; then
+      tac="tac"
+    else
+      tac="tail -r"
+    fi
+    BUFFER=$(\history -n 1 | eval $tac | awk '!a[$0]++' | peco --query "$LBUFFER")
         CURSOR=$#BUFFER
-    }
-    zle -N _peco-select-history
-    bindkey '^r' _peco-select-history
+  }
+  zle -N _peco-select-history
+  bindkey '^r' _peco-select-history
 
-    function _peco-ssh-select-host() {
-        local host
-        host=$(grep -iE '^host' ~/.ssh/config | awk '{print $2}' | peco)
-        if [ "$host" != '' ]; then
-            ssh $@ $host
-        fi
-    }
-    alias ssh-peco=_peco-ssh-select-host
+  function _peco-ssh-select-host() {
+    local host
+    host=$(grep -iE '^host' ~/.ssh/config | awk '{print $2}' | peco)
+    if [ "$host" != '' ]; then
+      ssh $@ $host
+    fi
+  }
+  alias ssh-peco=_peco-ssh-select-host
 fi
--- .zsh/tmux.zsh
+++ .zsh/tmux.zsh
@@ -1,32 +1,32 @@
 if _command_exists tmux; then
-    function tmux-change-prefix() {
-        local old_prefix=$(tmux list-keys | grep 'send-prefix' | tr -s ' ' | cut -d ' ' -f 2)
-        local new_prefix='C-b'
-        local user_prefix=''
+  function tmux-change-prefix() {
+    local old_prefix=$(tmux list-keys | grep 'send-prefix' | tr -s ' ' | cut -d ' ' -f 2)
+    local new_prefix='C-b'
+    local user_prefix=''
 
-        # Wait user input if prefix key is not specified
-        if [ "$1" = '' ]; then
-            printf "Select prefix key (default: C-b): C-"
-            read -k 1 user_prefix
-            printf "\n"
-            if [ "$user_prefix" = '' ]; then
-                new_prefix='C-b'
-            else
-                new_prefix="C-$user_prefix"
-            fi
-        else
-            new_prefix="C-${1:0:1}"
-        fi
+    # Wait user input if prefix key is not specified
+    if [ "$1" = '' ]; then
+      printf "Select prefix key (default: C-b): C-"
+      read -k 1 user_prefix
+      printf "\n"
+      if [ "$user_prefix" = '' ]; then
+        new_prefix='C-b'
+      else
+        new_prefix="C-$user_prefix"
+      fi
+    else
+      new_prefix="C-${1:0:1}"
+    fi
 
-        # Change prefix key
-        tmux unbind $old_prefix
-        tmux set -g prefix $new_prefix &> /dev/null
-        tmux bind $new_prefix send-prefix
+    # Change prefix key
+    tmux unbind $old_prefix
+    tmux set -g prefix $new_prefix &> /dev/null
+    tmux bind $new_prefix send-prefix
 
-        # Update status line
-        tmux refresh -S
+    # Update status line
+    tmux refresh -S
 
-        # Message
-        echo "Changed tmux prefix key: $old_prefix -> $new_prefix"
-    }
+    # Message
+    echo "Changed tmux prefix key: $old_prefix -> $new_prefix"
+  }
 fi
