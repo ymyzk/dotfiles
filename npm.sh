8cc768b727c2949cea9d5ec54dc9719013381049
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-07-30 23:23:55 +0900

    Add npm.sh

---
+++ npm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+npm install -g npm
+
+npm install -g babel
+npm install -g bower
+npm install -g generator-hubot
+npm install -g grunt-cli
+npm install -g jshint
+npm install -g typescript
+npm install -g yo
