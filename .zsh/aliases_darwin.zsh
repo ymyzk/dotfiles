a097805e959bd74a86b998a1b04d7c99197872d6
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-03-08 09:56:13 +0900

    Update aliases_darwin.zsh

--- .zsh/aliases_darwin.zsh
+++ .zsh/aliases_darwin.zsh
@@ -1,3 +1,5 @@
+# Aliases
+
 alias ls='ls -G'
 alias ll='ls -G -al'
 
@@ -7,15 +9,16 @@ alias firefox='open -a Firefox'
 alias safari='open -a Safari'
 alias cot='open -a CotEditor'
 
-alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
-alias ios-simulator='open $(xcode-select -p)/Applications/iOS\ Simulator.app'
-alias rm-open-in-duplicates="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
-alias osx-sleep="osascript -e 'tell application \"Finder\" to sleep'"
+# Suffix aliases
 
 alias -s html=open
 alias -s {png,jpg,bmp,pdf,PNG,JPG,BMP,PDF}='open -a Preview'
 
-# iOS Device UDID
-function show-udids() {
-    system_profiler SPUSBDataType | sed -n -e '/iPad/,/Serial/p' -e '/iPhone/,/Serial/p' | grep "Serial Number:" | awk -F ": " '{print $2}'
-}
+# Useful commands
+
+alias airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
+alias ios-simulator='open $(xcode-select -p)/Applications/Simulator.app'
+alias watch-simulator='open $(xcode-select -p)/Applications/Simulator\ \(Watch\).app'
+alias rm-open-in-duplicates="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
+alias osx-sleep="osascript -e 'tell application \"Finder\" to sleep'"
+alias show-udids='system_profiler SPUSBDataType | sed -n -e "/iPad/,/Serial/p" -e "/iPhone/,/Serial/p" | grep "Serial Number:" | awk -F ": " "{print $2}"'
