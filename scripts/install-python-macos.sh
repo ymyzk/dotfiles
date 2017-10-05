8828845fff0e59a18ab96f258cf88a3b8134a57a
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2017-10-05 17:16:25 +0900

    Update install-python-macos.sh

--- scripts/install-python-macos.sh
+++ scripts/install-python-macos.sh
@@ -5,20 +5,22 @@ set +u
 version=$1
 set -u
 if [ -z "$version" ]; then
+  echo "usage: $0 <version>"
   exit 1
 fi
 
 current_dir=$(pwd)
 temp_dir=$(mktemp -d)
 install_dir="/opt/python/$version"
+short_version=$(echo "$version" | cut -f 1 -d a | cut -f 1 -d b | cut -f 1 -d r)
 
-LDFLAGS=-L/usr/local/opt/openssl/lib
-CPPFLAGS=-I/usr/local/opt/openssl/include
+export CPPFLAGS="-I/usr/local/opt/openssl/include -I/usr/local/opt/zlib/include"
+export LDFLAGS="-L/usr/local/opt/openssl/lib -L/usr/local/opt/zlib/lib"
 
 echo "Using temp dir: $temp_dir"
 cd "$temp_dir"
 
-wget -O source.tar.xz "https://www.python.org/ftp/python/$version/Python-$version.tar.xz"
+wget -O source.tar.xz "https://www.python.org/ftp/python/$short_version/Python-$version.tar.xz"
 tar xf source.tar.xz
 cd "Python-$version"
 
