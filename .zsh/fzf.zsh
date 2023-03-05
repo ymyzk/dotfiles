38f3ce63b7c83ac321595cc5483da5115adf0f63
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2023-03-05 23:20:30 +0900

    Remove --no-sort from fzf history

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,6 +1,6 @@
 if _command_exists fzf; then
   function _fzf-select-history() {
-    BUFFER=$(\history -n 1 | awk '!a[$0]++' | fzf --tac --no-sort --reverse --prompt="HISTORY> " --query="$LBUFFER")
+    BUFFER=$(\history -n 1 | awk '!a[$0]++' | fzf --tac --reverse --prompt="HISTORY> " --query="$LBUFFER")
     CURSOR=$#BUFFER
   }
   zle -N _fzf-select-history
