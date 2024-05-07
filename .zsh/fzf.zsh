158064a80a843e4f0d045f4b7c243ea0ebb61f07
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2024-05-07 21:57:28 +0900

    Update fzf select history

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,9 +1,3 @@
 if _command_exists fzf; then
-  function _fzf-select-history() {
-    # Remove index from history, remove duplicates, then trigger fzf
-    BUFFER=$(\history -n 1 | awk '!a[$0]++' | fzf --tac --reverse --tiebreak=index --prompt="HISTORY> " --query="$LBUFFER")
-    CURSOR=$#BUFFER
-  }
-  zle -N _fzf-select-history
-  bindkey '^r' _fzf-select-history
+  eval "$(fzf --zsh)"
 fi
