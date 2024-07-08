1257b1690750f4c11f443ae60f19aa160d770a8b
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2024-07-08 09:57:18 +0900

    Simplify prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -4,16 +4,6 @@ local p_uh="%(?.%F{green}.%F{magenta})%n@%m%f${WINDOW+[$WINDOW]}"
 local p_cd="%F{cyan}%~%f"
 # prefix (root-># user->$)
 local p_pr="%(!,#,$)"
-# remote host
-local p_rh=''
 
-# SSH 経由で接続しているときはリモートホストの情報を追加
-if [[ -n "${REMOTEHOST}${SSH_CONNECTION}" ]]; then
-    local rh=`who am i | sed 's/.*(\(.*\)).*/\1/'`
-    rh=${rh#localhost:}
-    rh=${rh%%.*}
-    p_rh=" %F{yellow}(${rh})%f"
-fi
-
-PROMPT="$p_uh$p_rh: $p_cd
+PROMPT="$p_uh: $p_cd
 $p_pr "
